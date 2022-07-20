192c98263cff5aea4b6f7face69bf4fc372353bd
Adeazil12 <107722476+Adeazil12@users.noreply.github.com>
2022-07-20 23:09:03 +0700

    Update infoalpi.js

--- plugins/infoalpi.js
+++ plugins/infoalpi.js
@@ -1,13 +1,13 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI Alpi* ´ˎ˗
-│ ✎ _Nama_ : Alpi
-│ ✎ _Sekolah_ : SMA 1 BANTARBOLANG
-│ ✎ _Umur_ : 17
+│ ✎ _Nama_ : Ademods
+│ ✎ _Sekolah_ : SMA 1 BANJAREJO
+│ ✎ _Umur_ : 20
 │ ✎ _Asal_ : Jatim
-│ ✎ _Status_ : Pelajar SMA
+│ ✎ _Status_ : Pelajar lulus Bro
 │ ✎ _WhatsApp_ :
-│    wa.me/6285713137903
+│    wa.me/628980330708
 ╰──────────────
 `.trim(), m)
 }
